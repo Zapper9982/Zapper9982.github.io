1388a3d373be2b4ad56a5d1a7d401ce7afb035f0
Zapper9982 <emailtotanmay@gmail.com>
2024-07-18 00:16:24 +0530

    final-change-02

--- components/TechStack.tsx
+++ components/TechStack.tsx
@@ -8,7 +8,7 @@ export function TechStack() {
   return (
   <div className="flex  flex-col justify-center items-center mt-16" id="tech">
   <h1 className="text-6xl font-bold">My <span className="text-orange-300 ">Ultimate</span> Tech Stack</h1>
-  <p className="text-lime-100">scroll down on the images to see all of them</p>
+  <p className="text-sm">scroll down on the images to see all of them</p>
   <ParallaxScroll images={stack} />
   </div>);
 }
