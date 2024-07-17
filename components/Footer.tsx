978fb88782242eeda2295b9a722a7a24093f5c76
Zapper9982 <emailtotanmay@gmail.com>
2024-07-18 00:42:11 +0530

    final-touch-03

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -19,7 +19,7 @@ const Footer = () => {
         <p className='md:text-base text-sm md:font-normal font-extralight'>Copyright © 2024 Tanmay Deobhankar</p>
         <div className='flex items-center md:gap-3 gap-6 '>{socialMedia.map((profile) =>(
             <div key={profile.id} className='w-10 h-10 cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-200 rounded-lg border border-black-300 mb-8'>
-               <a href={profile.navigate}> <Image  src={profile.img} alt={profile.id} width={20} height={20}/></a>
+               <a href={profile.navigate}> <Image  src={profile.img} alt={profile.id} width='40' height='40'/></a>
 
             </div>
         ))}</div>
