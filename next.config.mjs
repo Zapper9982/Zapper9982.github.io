e89c086b28d83032cda78690fa0626b8bbac1030
Zapper9982 <emailtotanmay@gmail.com>
2025-12-13 10:53:03 +0530

    fix(build)

--- next.config.mjs
+++ next.config.mjs
@@ -17,9 +17,6 @@ const nextConfig = {
     swcMinify: true,
     compiler: {
         removeConsole: process.env.NODE_ENV === 'production'
-    },
-    experimental: {
-        optimizeCss: true,
     }
 };
 
